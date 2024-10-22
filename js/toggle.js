d1e9c38ed8a7e9689ad8d270f07976653acf1d60
Jong1 <abgf0103@gmail.com>
2024-10-22 14:38:01 +0900

    Add toggle btn

---
+++ js/toggle.js
@@ -0,0 +1,22 @@
+const toggle = document.getElementById("mode-toggle");
+const body = document.body;
+
+toggle.addEventListener("change", function () {
+    if (this.checked) {
+        body.classList.add("block-mode");
+        localStorage.setItem("blockMode", "enabled");
+    } else {
+        body.classList.remove("block-mode");
+        localStorage.setItem("blockMode", "disabled");
+    }
+});
+
+// check for saved user preference, if any, on load of the website
+document.addEventListener("DOMContentLoaded", (event) => {
+    const blockMode = localStorage.getItem("blockMode");
+
+    if (blockMode === "enabled") {
+        body.classList.add("block-mode");
+        toggle.checked = true;
+    }
+});
